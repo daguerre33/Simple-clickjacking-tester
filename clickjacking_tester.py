89fca25c5b8ff0c7cd7fb43f8630aa498f36a358
daguerre33 <92821707+daguerre33@users.noreply.github.com>
2022-09-22 22:35:30 +0200

    Create clickjacking_tester.py

---
+++ clickjacking_tester.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+def clickjacking_test(url):
+        request = urllib.request.Request(url)
+        opener = urllib.request.urlopen(request)
+        print(opener.status, opener.reason)
+        if opener.headers.__contains__("X-Frame-Options"):
+                print("Framing is not allowed")
+        else:
+                print("Framing is allowed")
+
+if __name__ == "__main__":
+        host = input("Host url (eg. http://example.com): ")
+        clickjacking_test(host)
