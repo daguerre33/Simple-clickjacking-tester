5290f86df9c4d749f589c1e71f2e2965fb2d92b5
daguerre33 <92821707+daguerre33@users.noreply.github.com>
2022-09-24 21:16:52 +0200

    Update clickjacking_tester.py

--- clickjacking_tester.py
+++ clickjacking_tester.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 
 import urllib.request
+from urllib.error import URLError
+from urllib.error import HTTPError
 
 def clickjacking_test(url):
-        request = urllib.request.Request(url)
-        opener = urllib.request.urlopen(request)
+        try:
+                request = urllib.request.Request(url)
+                opener = urllib.request.urlopen(request)
+        except URLError as error:
+                print(error)
+        except HTTPError as error:
+                print(error)
+
+        content = opener.headers
         print(opener.status, opener.reason)
-        if opener.headers.__contains__("X-Frame-Options"):
+        if content.__contains__("X-Frame-Options"):
                 print("Framing is not allowed")
         else:
                 print("Framing is allowed")
+
+        resp_print = " "
+        while resp_print not in ["y", "n"]:
+                resp_print = input("Response shall be printed? (y/n) ")
+                if resp_print == "y":
+                        print(content)
+                elif resp_print == "n":
+                        print("Bye!")
+                else:
+                        print("Please, choose 'y' or 'n'!")
+
 
 if __name__ == "__main__":
         host = input("Host url (eg. http://www.example.com): ")
         clickjacking_test(host)
 
-        
